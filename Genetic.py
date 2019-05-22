d1c776d32e052ee366a5cf368c58ede8e8b0d51f
Amirhossein Azimyzadeh <39561409+AmirhosseinAzimyzadeh@users.noreply.github.com>
2019-05-23 00:33:27 +0430

    add some method

--- Genetic.py
+++ Genetic.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 
 class Individual:
@@ -6,14 +7,15 @@ class Individual:
         self.x = x
         self.y = y
         self.f = f(x, y)
-        self.fitness = 0
-
-    def set_fitness(self, fit):
-        self.fitness = fit
 
 
 def fitness(population):
-    return
+    # Cross-in-Tray function always return negative value
+    # prob. = x.sum * 100
+    sum_of_values = 0
+    for p in population:
+        sum_of_values += p.f
+    return [(p.f / sum_of_values) * 100 for p in population]
 
 
 def f(x, y):
@@ -26,4 +28,55 @@ def f(x, y):
     return result
 
 
+def first_generation(n_of_population):  # return first generation
+    return [Individual(random.uniform(-10, 10), random.uniform(-10, 10)) for p in range(n_of_population)]
+
+
+def child_creator(parent1, parent2, mutation_prob):
+    mutation_prob = 0.1
+    x_from_parent1 = (random.uniform(-1, 1) < 0)
+    child1_x = 0
+    child1_y = 0
+    child2_x = 0
+    child2_x = 0
+
+    if x_from_parent1:
+        child1_x = parent1.x
+        child1_y = parent2.y
+        child2_x = parent1.y
+        child2_y = parent2.x
+    else:
+        child1_x = parent2.x
+        child1_y = parent1.y
+        child2_x = parent1.x
+        child2_y = parent2.y
+
+    prob = random.uniform(1 - mutation_prob, 1 + mutation_prob)
+    child1 = Individual(child1_x * prob, child1_y) if (random.uniform(-1, 1) < 0) else Individual(child1_x,
+                                                                                                  child1_y * prob)
+    prob = random.uniform(1 - mutation_prob, 1 + mutation_prob)
+    child2 = Individual(child2_x * prob, child2_y) if (random.uniform(-1, 1) < 0) else Individual(child2_x,
+                                                                                                  child2_y * prob)
+
+    return [child1, child2]
+
+
+# -------------------------- print functions ----------------------------
+def print_population(population):
+    for i in range(len(population)):
+        print('(X:' + str(population[i].x) + ',Y:' + str(population[i].y) + ')   \tF =' + str(population[i].f))
+
+
+def print_prob(prob_array):
+    for i in prob_array:
+        print(i)
+
+
+# ------------------------------------------------------------------------
+
 print(f(-5, -6))
+number_of_generation = int(input('number of generation :'))
+number_of_population = int(input('number of population :'))
+print_population(child_creator(Individual(-6, 5), Individual(-10, 1), 0.1))
+print('___________')
+print_prob(fitness(first_generation(number_of_population)))
